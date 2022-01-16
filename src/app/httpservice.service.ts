6a87c8fdf2504ccc630c27703255269a96c516af
Rudolph Nativel <rudolph.nativel@gmail.com>
2022-01-16 15:42:45 +0100

    changement du port 3000 en 8080

--- src/app/httpservice.service.ts
+++ src/app/httpservice.service.ts
@@ -12,17 +12,17 @@ export class HttpserviceService  {
   constructor(private client: HttpClient) { }
 
   getArticles():Observable<Array<Article>>{
-    return this.client.get<Array<Article>>(`http://localhost:3000/articles`)
+    return this.client.get<Array<Article>>(`http://localhost:8080/articles`)
   }
   deleteArticel(id:number) :Observable<Article>{
-    console.log(`http://localhost:3000/articles/${id}`);
-    return this.client.delete<Article>(`http://localhost:3000/articles/${id}`)
+    console.log(`http://localhost:8080/articles/${id}`);
+    return this.client.delete<Article>(`http://localhost:8080/articles/${id}`)
   }
 
   addArticle(article:Article):Observable<Article>{
-   return this.client.post<Article>(`http://localhost:3000/articles`,{title:article.title,link:article.link,votes:article.votes});
+   return this.client.post<Article>(`http://localhost:8080/articles`,{title:article.title,link:article.link,votes:article.votes});
   }
   UpdateArticle(article:Article):Observable<Article>{
-    return this.client.put<Article>(`http://localhost:3000/articles/${article.id}`,{title:article.title,link:article.link,votes:article.votes});
+    return this.client.put<Article>(`http://localhost:8080/articles/${article.id}`,{title:article.title,link:article.link,votes:article.votes});
    }
 }
